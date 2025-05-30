e570e66fe29ebcf9795c6a313d98734d89ddb37f
Kevin Hildebrand <kevin.hildebrand@gmail.com>
2025-05-30 19:37:06 -0400

    chore: Update NPM configuration and clean up unused files

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Wrapper script to run the fullstack development environment
-
-# Forward to the main script in the scripts directory
-exec ./scripts/run-fullstack.sh
--- stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Wrapper script to stop the fullstack development environment
-
-# Forward to the main script in the scripts directory
-exec ./scripts/stop-fullstack.sh
